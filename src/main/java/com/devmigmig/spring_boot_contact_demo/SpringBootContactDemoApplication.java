274b50d4bf033a59c697d7d0b82ae8e27a889bc5
devmigmig <roymiguelcrucillo@gmail.com>
2025-11-28 18:35:11 +0800

    chore(init): initialize Spring Boot Contact Demo project

---
+++ src/main/java/com/devmigmig/spring_boot_contact_demo/SpringBootContactDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devmigmig.spring_boot_contact_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootContactDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootContactDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmigmig/spring_boot_contact_demo/SpringBootContactDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmigmig.spring_boot_contact_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContactDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
