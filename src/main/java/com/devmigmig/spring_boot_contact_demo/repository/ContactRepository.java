587e517d5de7133f2d307b4ffff075f01808fbc4
devmigmig <roymiguelcrucillo@gmail.com>
2025-11-28 19:51:05 +0800

    feat(repository): update ContactRepository.java

--- src/main/java/com/devmigmig/spring_boot_contact_demo/repository/ContactRepository.java
+++ src/main/java/com/devmigmig/spring_boot_contact_demo/repository/ContactRepository.java
@@ -1,5 +1,25 @@
 package com.devmigmig.spring_boot_contact_demo.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.devmigmig.spring_boot_contact_demo.pojo.Contact;
+
+@Repository
 public class ContactRepository {
-    
+    private List<Contact> contacts = new ArrayList<>();
+
+    public List<Contact> getContacts(){
+        return contacts;
+    }
+
+    public Contact geContact(int index){
+        return contacts.get(index);
+    }
+
+    public void saveContact(Contact contact){
+        contacts.add(contact);
+    }
 }
