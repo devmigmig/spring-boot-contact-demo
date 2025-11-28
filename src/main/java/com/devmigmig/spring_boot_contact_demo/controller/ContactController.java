ee926e76934fa221e53061193eef7fe4cdb9f6a6
devmigmig <roymiguelcrucillo@gmail.com>
2025-11-29 01:22:13 +0800

    feat(controller): create getContacts

--- src/main/java/com/devmigmig/spring_boot_contact_demo/controller/ContactController.java
+++ src/main/java/com/devmigmig/spring_boot_contact_demo/controller/ContactController.java
@@ -1,5 +1,24 @@
 package com.devmigmig.spring_boot_contact_demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devmigmig.spring_boot_contact_demo.pojo.Contact;
+import com.devmigmig.spring_boot_contact_demo.service.ContactService;
+
+
+@RestController
 public class ContactController {
-    
+
+    @Autowired
+    private ContactService contactService;
+
+    public ResponseEntity<List<Contact>> getContacts(){
+        List<Contact> contacts = contactService.getContacts();
+        return new ResponseEntity<>(contacts, HttpStatus.OK);
+    }
 }
