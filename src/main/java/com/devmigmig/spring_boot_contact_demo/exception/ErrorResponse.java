c5ac1439d96e2301ab9cf56b33e2cc4eeba07a5b
devmigmig <roymiguelcrucillo@gmail.com>
2025-12-07 22:18:27 +0800

    feat(exception): add JSON date format to timestamp in ErrorResponse

--- src/main/java/com/devmigmig/spring_boot_contact_demo/exception/ErrorResponse.java
+++ src/main/java/com/devmigmig/spring_boot_contact_demo/exception/ErrorResponse.java
@@ -3,10 +3,12 @@ package com.devmigmig.spring_boot_contact_demo.exception;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 public class ErrorResponse {
 
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timestamp;
     private List<String> message;
 
