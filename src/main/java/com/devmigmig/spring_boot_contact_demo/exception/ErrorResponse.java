be4ab3893f34cabb327c7ab82f39b4fc922f59f7
devmigmig <roymiguelcrucillo@gmail.com>
2025-12-07 22:16:30 +0800

    feat (exception): update ErrorResponse.java

--- src/main/java/com/devmigmig/spring_boot_contact_demo/exception/ErrorResponse.java
+++ src/main/java/com/devmigmig/spring_boot_contact_demo/exception/ErrorResponse.java
@@ -1,5 +1,33 @@
 package com.devmigmig.spring_boot_contact_demo.exception;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+
 public class ErrorResponse {
+
+
+    private LocalDateTime timestamp;
+    private List<String> message;
+
+    public ErrorResponse(List<String> message){
+        this.timestamp = LocalDateTime.now();
+        this.message = message;
+    }
+
+    public LocalDateTime getTimestamp(){
+        return this.timestamp;
+    }
     
+    public void setTimstamp(LocalDateTime timestamp){
+        this.timestamp = timestamp;
+    }
+
+    public List<String> getMessage(){
+        return this.message;
+    }
+
+    public void setMessage(List<String> message){
+        this.message = message;
+    }
 }
