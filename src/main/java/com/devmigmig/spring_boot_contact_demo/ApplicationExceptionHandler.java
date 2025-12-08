359b1edf38d572336c16d6db329250efb849e045
devmigmig <roymiguelcrucillo@gmail.com>
2025-12-08 13:46:57 +0800

    feat (exception): add handleContactNotFoundException

--- src/main/java/com/devmigmig/spring_boot_contact_demo/ApplicationExceptionHandler.java
+++ src/main/java/com/devmigmig/spring_boot_contact_demo/ApplicationExceptionHandler.java
@@ -1,5 +1,20 @@
 package com.devmigmig.spring_boot_contact_demo;
 
-public class ApplicationExceptionHandler {
+import java.util.Arrays;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.devmigmig.spring_boot_contact_demo.exception.ContactNotFoundException;
+import com.devmigmig.spring_boot_contact_demo.exception.ErrorResponse;
+
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
     
+    @ExceptionHandler(ContactNotFoundException.class)
+    public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException ex){
+        ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getLocalizedMessage()));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
