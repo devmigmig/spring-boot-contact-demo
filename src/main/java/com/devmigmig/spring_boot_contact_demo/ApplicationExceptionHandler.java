30bfe96f4993466dd7e5e050fd8f2030e530f7fc
devmigmig <roymiguelcrucillo@gmail.com>
2025-12-08 13:50:46 +0800

    feat (exception): add handleMethodArgumentNotValid

--- src/main/java/com/devmigmig/spring_boot_contact_demo/ApplicationExceptionHandler.java
+++ src/main/java/com/devmigmig/spring_boot_contact_demo/ApplicationExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.devmigmig.spring_boot_contact_demo;
 
+import java.net.http.HttpHeaders;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.devmigmig.spring_boot_contact_demo.exception.ContactNotFoundException;
@@ -16,5 +22,15 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler 
     public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException ex){
         ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getLocalizedMessage()));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            org.springframework.http.HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach(
+            (error) -> errors.add(error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
     }
 }
