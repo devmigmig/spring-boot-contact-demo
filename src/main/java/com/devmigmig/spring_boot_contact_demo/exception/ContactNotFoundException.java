55a6c7d509ae367d85b6d0cbec6ccc5be7d237df
devmigmig <roymiguelcrucillo@gmail.com>
2025-12-08 13:29:02 +0800

    feat (exception): update ContactNotFoundException

--- src/main/java/com/devmigmig/spring_boot_contact_demo/exception/ContactNotFoundException.java
+++ src/main/java/com/devmigmig/spring_boot_contact_demo/exception/ContactNotFoundException.java
@@ -1,5 +1,7 @@
 package com.devmigmig.spring_boot_contact_demo.exception;
 
-public class ContactNotFoundException {
-    
+public class ContactNotFoundException extends RuntimeException {
+    public ContactNotFoundException(String id){
+        super("The id '"+id+"' does not exist in our records");
+    }
 }
